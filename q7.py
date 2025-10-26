44451cb5e34f3bacb73296a9478834b778e738e3
SS <158577953+SupritamSarkar@users.noreply.github.com>
2025-10-26 22:47:58 +0530

    Add Java implementation for finding large neighbors

--- q7.py
+++ q7.py
@@ -7,3 +7,33 @@ for i in range(1,n-1):
 	if arr[i]>arr[i+1] and arr[i]>arr[i-1]:
 		s+=arr[i]
 print(s)
+
+
+-------java ----------
+
+import java.util.*;
+
+public class large_neigh {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if ((i == 0 && arr[i] > arr[i + 1]) ||
+                    (i == n - 1 && arr[i] > arr[i - 1]) ||
+                    (i > 0 && i < n - 1 && arr[i] > arr[i - 1] && arr[i] > arr[i + 1])) {
+                sum += arr[i];
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
