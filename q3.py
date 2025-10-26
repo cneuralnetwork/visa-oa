d27d91730efef8dfb1a5e1f665f4cc32487fa92d
SS <158577953+SupritamSarkar@users.noreply.github.com>
2025-10-26 22:32:08 +0530

    Add Java implementation for counting digit pairs

--- q3.py
+++ q3.py
@@ -24,3 +24,54 @@ for g in grps.values():
 			patt=i[:j]+'*'+i[j+1:]
 			ans+=cnt[patt]-1
 return ans//2
+
+
+
+------------------java-----------------
+
+import java.util.*;
+
+public class digitPair {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(difference(arr[i],arr[j])){
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+        
+    }
+
+    static boolean difference(int a,int b){
+        String s1 = String.valueOf(a);
+        String s2 = String.valueOf(b);
+        int diff=0;
+
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s2.charAt(i) != s1.charAt(i)) {
+                diff++;
+            }
+            if(diff>1){
+                return false;
+            }
+        }
+        return diff==1;
+    }
+}
