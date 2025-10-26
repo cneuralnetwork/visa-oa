ad7e0d51397e07bf397eeb37dccb66175b7131a2
SS <158577953+SupritamSarkar@users.noreply.github.com>
2025-10-27 00:26:13 +0530

    add Java implementation

--- q8.py
+++ q8.py
@@ -50,3 +50,57 @@ if chk:
 	print(B[j])
 else:
 	print(-1)
+
+
+-----------java------------
+
+import java.util.*;
+
+public class mission {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int mission = sc.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[m];
+
+
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i=0;i<m;i++){
+            b[i] = sc.nextInt();
+        }
+
+        int i=0,j=0;
+        int last = -1;
+
+        while(i<n && j<m){
+            if(b[j]>a[i]){
+                last = b[j];
+                mission--;
+                i++;
+                j++;
+                if(mission==0){
+                     System.out.println(last);
+                     break;
+                }
+            }
+            else{
+                j++;
+            }
+        }
+        if(mission>0){
+            System.out.println(-1);
+        }
+
+        
+        
+    }
+}
+
+
+
+
